1244cb719e52f9fa87044f627118fa95cf9ce9c7
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 20:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ const driversByName = function(drivers){
 const totalRevenue = function(drivers){
   return drivers.reduce((s, d) => s + d.revenue );
 }
+
+const averageRevenue =  function (drivers){
+  return totalRevenue(drivers)/drivers.length
+}
