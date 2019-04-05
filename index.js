d063888ce1155e51507107fb8c98aab36b1643e5
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 20:09:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers){
+  drivers.forEach(driver => console.log(driver.name));
+}
+
+function logDriversByHometown(drivers, location){
+  logDriverNames(drivers.filter(driver => driver.hometown.toUpperCase() === location.toUpperCase()))
+}
+
+const driversByRevenue = function(drivers){
+  const arr = [...drivers];
+  return arr.sort((d1, d2) => d1.revenue > d2.revenue );
+}
+
+const driversByName = function(drivers){
+  const arr = [...drivers];
+  return arr.sort((d1, d2) => d1.name.localeCompare(d2.name) );
+}
+
+const totalRevenue = function(drivers){
+  return drivers.reduce(d => d.revenue);
+}
