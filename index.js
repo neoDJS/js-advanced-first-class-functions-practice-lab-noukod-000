488877622f7a78841755c371df0f600df1f34727
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 20:22:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers){
+  drivers.forEach(driver => console.log(driver.name));
+}
+
+function logDriversByHometown(drivers, location){
+  logDriverNames(drivers.filter(driver => driver.hometown.toUpperCase() === location.toUpperCase()))
+}
+
+const driversByRevenue = function(drivers){
+  const arr = [...drivers];
+  return arr.sort((d1, d2) => d1.revenue > d2.revenue );
+}
+
+const driversByName = function(drivers){
+  const arr = [...drivers];
+  return arr.sort((d1, d2) => d1.name.localeCompare(d2.name) );
+}
+
+const totalRevenue = function(drivers){
+  return drivers.reduce((s, d) => {return s + d.revenue}, 0 );
+}
+
+const averageRevenue =  function (drivers){
+  return totalRevenue(drivers)/drivers.length;
+}
